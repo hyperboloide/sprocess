35cb7a82c54b0c412c353fb04a7e489d786b413a
Frederic Delbos <fred.delbos@gmail.com>
2015-02-10 15:58:52 +0100

    add checksum

---
+++ checksum.go
@@ -0,0 +1,84 @@
+// 
+// checksum.go
+// 
+// Created by Frederic DELBOS - fred@hyperboloide.com on Feb 10 2015.
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+// 
+
+package sprocess
+
+import (
+	"errors"
+	"io"
+	"crypto/sha1"
+	"crypto/md5"
+	"fmt"
+	"hash"
+)
+
+type CheckSum struct {
+	Hash string
+	Name     string
+}
+
+func (c *CheckSum) GetName() string {
+	return c.Name
+}
+
+func (c *CheckSum) Start() error {
+	switch c.Hash {
+	case "", "md5", "sha1":
+	default:
+		return errors.New("Hash '"+c.Hash+"' is not supported")		
+	}
+	return nil
+}
+
+func (c *CheckSum) Encode(r io.Reader, w io.Writer, d *Data) error {
+	var h hash.Hash
+	switch c.Hash {
+	case "", "sha1":
+		h = sha1.New()
+	case "md5":
+		h = md5.New()
+	default:
+		return errors.New("Hash '"+c.Hash+"' is not supported")
+	}
+
+	reader := io.TeeReader(r, h)
+	_, err := io.Copy(w, reader)
+	if err != nil {
+		return err
+	}
+	d.Set(c.GetName(), fmt.Sprintf("%x", h.Sum(nil)))
+	return nil
+}
+
+func (c *CheckSum) Decode(r io.Reader, w io.Writer, d *Data) error {
+	var h hash.Hash
+	switch c.Hash {
+	case "", "sha1":
+		h = sha1.New()
+	case "md5":
+		h = md5.New()
+	default:
+		return errors.New("Hash '"+c.Hash+"' is not supported")
+	}
+
+	value, err := d.Get(c.GetName())
+	if err != nil {
+		return err
+	}
+	
+	reader := io.TeeReader(r, h)
+	_, err = io.Copy(w, reader)
+	if err != nil {
+		return err
+	}
+	if fmt.Sprintf("%x", h.Sum(nil)) != value {
+		return errors.New("CheckSums do not match")
+	}
+	return nil
+}
+
---
+++ checksum_test.go
@@ -0,0 +1,51 @@
+package sprocess_test
+
+import (
+	. "github.com/hyperboloide/sprocess"
+	"bytes"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Checksum", func() {
+
+	testBin := genBlob(1 << 22)
+	out1 := new(bytes.Buffer)
+	data := NewData()
+
+	chck := &CheckSum{
+		Name: "chck",
+	}
+	
+	It("should Encode", func() {
+		Ω(chck.Start()).To(BeNil())
+		Ω(chck.Encode(
+			bytes.NewReader(testBin),
+			out1,
+			data)).To(BeNil())
+		output := out1.Bytes()
+		Ω(bytes.Equal(output, testBin)).To(BeTrue())
+		d := data.Export()
+		_, exists := d["chck"]
+		Ω(exists).To(BeTrue())
+	})
+
+	out2 := new(bytes.Buffer)
+	It("should Decode", func() {
+		Ω(chck.Decode(
+			bytes.NewReader(out1.Bytes()),
+			out2,
+			data)).To(BeNil())
+		Ω(bytes.Equal(out2.Bytes(), testBin)).To(BeTrue())
+		d := data.Export()
+		_, exists := d["chck"]
+		Ω(exists).To(BeTrue())
+
+	})
+	
+	It("should not Decode if mismatch", func() {
+		out := new(bytes.Buffer)
+		Ω(chck.Decode(testFileReader(), out, data)).ToNot(BeNil())
+	})
+	
+})
