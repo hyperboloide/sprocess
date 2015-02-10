2c6bdb023a9ff8ef95c7bfd8ab44888606d83180
Frederic Delbos <fred.delbos@gmail.com>
2015-02-10 15:59:58 +0100

    add size encoder/decoder

---
+++ size.go
@@ -0,0 +1,41 @@
+// 
+// size.go
+// 
+// Created by Frederic DELBOS - fred@hyperboloide.com on Feb 10 2015.
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+// 
+
+package sprocess
+
+import (
+	"io"
+	"errors"
+)
+
+type Size struct {
+	Name  string
+}
+
+func (s *Size) GetName() string {
+	return s.Name
+}
+
+func (s *Size) Start() error {
+	if s.Name == "" {
+		return errors.New("Size must have a name")
+	}
+	return nil
+}
+
+func (s *Size) Encode(r io.Reader, w io.Writer, d *Data) error {
+	size, err := io.Copy(w, r)
+	d.Set(s.GetName(), size)
+	return err
+}
+
+func (s *Size) Decode(r io.Reader, w io.Writer, d *Data) error {
+	size, err := io.Copy(w, r)
+	d.Set(s.GetName(), size)
+	return err
+}
---
+++ size_test.go
@@ -0,0 +1,48 @@
+package sprocess_test
+
+import (
+	. "github.com/hyperboloide/sprocess"
+	"bytes"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Size", func() {
+
+	var res int64
+	res = 1 << 22
+	testBin := genBlob(int(res))
+	out1 := new(bytes.Buffer)
+	data := NewData()
+
+	size := &Size{
+		Name: "size",
+	}
+
+	It("should Encode", func() {
+		Ω(size.Start()).To(BeNil())
+		Ω(size.Encode(
+			bytes.NewReader(testBin),
+			out1,
+			data)).To(BeNil())
+		Ω(bytes.Equal(out1.Bytes(), testBin)).To(BeTrue())
+		s, err := data.Get("size")
+		Ω(err).To(BeNil())
+		Ω(s).To(Equal(res))
+	})
+
+	out2 := new(bytes.Buffer)
+	It("should Decode", func() {
+		Ω(size.Decode(
+			bytes.NewReader(out1.Bytes()),
+			out2,
+			data)).To(BeNil())
+		Ω(bytes.Equal(out2.Bytes(), testBin)).To(BeTrue())
+		s, err := data.Get("size")
+		Ω(err).To(BeNil())
+		Ω(s).To(Equal(res))
+
+	})
+
+	
+})
